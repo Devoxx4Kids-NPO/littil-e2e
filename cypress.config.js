92d16aa767527805f0c5bf6d850aaa0ceab4f64d
MarcelWildenburg <marcel.wildenburg.68@gmail.com>
2023-02-19 14:50:10 +0100

    Moved the environment variables to cypres.env.json
    Moved the enironement variables from .env to cypres.env.json so you can test with cypress in a docker container and with a locally installed cypress.

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,7 @@
 module.exports = {
   e2e: {
-    env : {
-       frontend_url: "http://localhost:80",
-       auth0_tenant: "TODO",
-       
-       user_school1_username: "e2etestschool1@littil.org",
-       user_school1_password: "TODO",
-       user_school2_username: "e2etestschool2@littil.org",
-       user_school2_password: "TODO",
-
-       user_docent1_username: "e2etestdocent1@littil.org",
-       user_docent1_password: "TODO",
-       user_docent2_username: "e2etestdocent2@littil.org",
-       user_docent2_password: "TODO"
-       
-    },
     experimentalStudio: false,
-    video: false,
+    video: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
